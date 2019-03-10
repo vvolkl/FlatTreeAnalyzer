b96ccaee6158340a65839380213b0bad4d5e3c17
Valentin Volkl <valentin.volkl@cern.ch>
2019-03-10 17:05:46 +0000

    packaging

--- init.sh
+++
@@ -1,4 +0,0 @@
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_88/Python/2.7.13/x86_64-slc6-gcc62-opt/Python-env.sh
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_88/ROOT/6.08.06/x86_64-slc6-gcc62-opt/bin/thisroot.sh
-export PYTHONPATH=$PYTHONPATH:/cvmfs/sft.cern.ch/lcg/views/LCG_88/x86_64-slc6-gcc62-opt/lib/python2.7/site-packages/
-#export PYTHONPATH=$PYTHONPATH:/afs/cern.ch/user/h/helsens/FCCsoft/
